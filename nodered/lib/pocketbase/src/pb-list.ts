7f73f85a6f05233bc7e64d303e5b1787374b745c
Benoit ADAM <b.adam.info@gmail.com>
2025-08-16 01:00:15 +0200

    add pb-list expand

--- nodered/lib/pocketbase/src/pb-list.ts
+++ nodered/lib/pocketbase/src/pb-list.ts
@@ -8,7 +8,8 @@ export interface PBListNodeDef extends NodeDef {
     perPage: number;
     filter: string;
     sort: string;
-    mode: 'page' | 'items' | 'split';
+    expand: string;
+    mode: 'page' | 'items' | 'split' | 'first';
 }
 
 module.exports = (RED: NodeAPI) => {
@@ -24,36 +25,50 @@ module.exports = (RED: NodeAPI) => {
                 const perPage = Number(def.perPage || msg.perPage || 50);
                 const filter = def.filter || msg.filter || '';
                 const sort = def.sort || msg.sort || '';
+                const expand = def.expand || msg.expand || '';
                 const mode = def.mode || msg.mode || 'page';
 
                 if (!isString(collection)) throw propError('Collection');
-                if (!isNumber(page) || page < 1) throw propError('Page');
-                if (!isNumber(perPage) || perPage < 1 || perPage > 99999) throw propError('Per Page');
+                if (mode !== 'first') {
+                    if (!isNumber(page) || page < 1) throw propError('Page');
+                    if (!isNumber(perPage) || perPage < 1 || perPage > 99999) throw propError('Per Page');
+                }
                 if (!isString(filter)) throw propError('Filter');
                 if (!isString(sort)) throw propError('Sort');
                 if (!isString(mode)) throw propError('Mode');
 
                 this.debug(`PB List: ${collection} page=${page} perPage=${perPage} filter='${filter}' sort='${sort}' mode=${mode}`);
 
-                const result = await pb.collection(collection).getList(page, perPage, {
-                    filter,
-                    sort,
-                    expand: msg.expand || ''
-                });
-
                 msg.pb = pb;
-                if (mode === 'page') {
-                    msg.payload = result;
-                    this.send(msg);
-                } else if (mode === 'items') {
-                    msg.payload = result.items;
-                    this.send(msg);
-                } else if (mode === 'split') {
-                    result.items.forEach((item) => {
-                        const newMsg = RED.util.cloneMessage(msg);
-                        newMsg.payload = item;
-                        this.send(newMsg);
+                
+                if (mode === 'first') {
+                    const result = await pb.collection(collection).getList(1, 1, {
+                        filter,
+                        sort,
+                        expand
                     });
+                    msg.payload = result.items[0] || null;
+                    this.send(msg);
+                } else {
+                    const result = await pb.collection(collection).getList(page, perPage, {
+                        filter,
+                        sort,
+                        expand
+                    });
+                    
+                    if (mode === 'page') {
+                        msg.payload = result;
+                        this.send(msg);
+                    } else if (mode === 'items') {
+                        msg.payload = result.items;
+                        this.send(msg);
+                    } else if (mode === 'split') {
+                        result.items.forEach((item) => {
+                            const newMsg = RED.util.cloneMessage(msg);
+                            newMsg.payload = item;
+                            this.send(newMsg);
+                        });
+                    }
                 }
 
             } catch (error) {
